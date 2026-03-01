6bdfc02195e56f53a70ad529229a9e12ad48bc2c
burns-edu <ryan.h.burns@edu.sait.ca>
2026-02-28 20:32:12 -0700

    Added Shorts section

---
+++ components/shorts.tsx
@@ -0,0 +1,140 @@
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+} from "react-native";
+import React from "react";
+import { VideoItem } from "./videoElement";
+import VideosData from "./videos.json";
+import { Ionicons } from "@expo/vector-icons";
+
+const Videos = VideosData as VideoItem[];
+const screenHeight = Dimensions.get("window").height;
+
+const SHORT_WIDTH = 400;
+const SHORT_HEIGHT = screenHeight - 60;
+
+const Shorts = () => {
+  const short = Videos[1];
+
+  return (
+    <View style={styles.container}>
+      {/* Wrapper to set screen width */}
+      <View style={styles.shortWrapper}>
+        {/* Short */}
+        <Image source={{ uri: short.thumbnail }} style={styles.short} />
+
+        {/* Header Overlay */}
+        <View style={styles.header}>
+          <View style={styles.leftIcons}>
+            <Ionicons name="arrow-back" size={24} color="white" />
+          </View>
+          <View style={styles.rightIcons}>
+            <Ionicons name="search" size={24} color="white" />
+            <Ionicons name="menu" size={24} color="white" />
+          </View>
+        </View>
+
+        {/* Sidebar Overlay */}
+        <View style={styles.sidebar}>
+          <Ionicons name="thumbs-up-outline" size={24} color="white" />
+          <Ionicons name="thumbs-down-outline" size={24} color="white" />
+          <Ionicons name="chatbox-ellipses-outline" size={24} color="white" />
+          <Ionicons name="share-outline" size={24} color="white" />
+          <Ionicons name="sync-outline" size={24} color="white" />
+        </View>
+
+        {/* Footer Overlay */}
+        <View style={styles.footer}>
+          {/* Channel Info */}
+          <View style={styles.channelInfo}>
+            <Image
+              source={{ uri: short.channelImage }}
+              style={styles.channelProfileImage}
+            />
+            <Text style={styles.channelName}>{short.channelName}</Text>
+          </View>
+          {/* Short Title */}
+          <Text style={styles.shortTitle}>{short.title}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Shorts;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  shortWrapper: {
+    width: SHORT_WIDTH,
+    height: SHORT_HEIGHT,
+    position: "relative",
+  },
+  header: {
+    position: "absolute",
+    top: 5,
+    left: 0,
+    right: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  footer: {
+    position: "absolute",
+    bottom: 40,
+    left: 20,
+    right: 20,
+  },
+  channelInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 5,
+  },
+  channelProfileImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 10,
+  },
+  short: {
+    width: SHORT_WIDTH,
+    height: SHORT_HEIGHT,
+    resizeMode: "cover",
+    borderRadius: 20,
+    marginRight: 10,
+  },
+  leftIcons: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  rightIcons: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  sidebar: {
+    position: "absolute",
+    right: 20,
+    bottom: 30,
+    flexDirection: "column",
+    gap: 20,
+  },
+  channelName: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 16,
+  },
+  shortTitle: {
+    color: "white",
+    fontSize: 14,
+  },
+});
